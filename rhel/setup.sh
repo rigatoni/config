b73a851d1a46ee8b95e2fa6ffd310b7f26d08a3a
Peter Mikitsh <peter.mikitsh@gmail.com>
2015-03-12 18:44:01 -0400

    update installation notes

--- rhel/setup.sh
+++ rhel/setup.sh
@@ -17,4 +17,32 @@ sudo yum install -y mongodb-org
 
 # install python3
 sudo yum install http://dl.iuscommunity.org/pub/ius/stable/Redhat/7/x86_64/ius-release-1.0-13.ius.el7.noarch.rpm
-sudo yum install python34u
+sudo yum install python34u python34u-dev
+
+# install node and build tools
+curl -sL https://rpm.nodesource.com/setup | bash -
+sudo yum install -y nodejs npm bzip2
+
+# install global node modules
+sudo npm install -g gulp bower
+
+# checkout linguine-node (develop branch) to /var/www/linguine-node-git
+sudo git clone -b develop https://github.com/rigatoni/linguine-node.git /var/www/linguine-node-git
+
+# checkout linguine-python (develop branch) to /var/www/linguine-python-git
+sudo git clone -b dev https://github.com/rigatoni/linguine-python /var/www/linguine-python-git
+
+# install node application-level dependencies
+cd /var/www/linguine-node-git
+sudo npm install
+sudo bower install --allow-root # when prompted, select "1"
+sudo gulp build
+
+# install RPM's that are required by python application level dependencies
+sudo yum install http://mirror.rit.edu/centos/7/os/x86_64/Packages/blas-devel-3.4.2-4.el7.x86_64.rpm
+sudo yum install http://mirror.rit.edu/centos/7/os/x86_64/Packages/lapack-devel-3.4.2-4.el7.x86_64.rpm
+
+# install python application-level dependencies
+cd /var/www/linguine-python-git
+sudo pip3.4 install -r requirements.txt # give this some time (...a lot of time)
+python3 -m textblob.download_corpora # install trained models
